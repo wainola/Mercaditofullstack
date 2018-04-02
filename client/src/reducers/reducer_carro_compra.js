b20f0a8a595dc35fb8350c27db52bead90ddaa45
Nicolás Riquelme <nrriquelme@gmail.com>
2018-04-02 20:50:22 -0300

    implemented the update over the state in case that the user select the same product

--- client/src/reducers/reducer_carro_compra.js
+++ client/src/reducers/reducer_carro_compra.js
@@ -5,10 +5,27 @@ export default function(state=[], action){
     //console.log("Reducer cart", action.payload);
     switch(action.type){
         case ADD_TO_CART:
-            console.log('reducer add_to_cart case');
-            console.log('state');
-            console.log(state);
-            console.log(action.payload);
+            // console.log('reducer add_to_cart case');
+            // console.log('state');
+            // console.log(state);
+            // console.log(action.payload);
+            console.log(_.some(state, ['id', action.payload.id]));
+            // IF THE ID ALREADY EXISTS, REPLACE FOR THE LAST PRODUCT
+            if(_.some(state, ['id', action.payload.id])){
+                let match = _.find(state, {id: action.payload.id});
+                let index = _.findIndex(state, {id: action.payload.id});
+                console.log('el indice en donde esta el elemento', index);
+                console.log('esta actualmente en el state', match);
+                console.log('queremos cambiarlo por', action.payload);
+                console.log('cambio:', _.merge(match, action.payload));
+                console.log('cambiando el state', state.splice(index, _.merge(match, action.payload)));
+                console.log('el nuevo estado es', state);
+                // let newState = _.merge(match, state);
+                // console.log('new match');
+                // console.log(newState);
+                
+                return [...state];
+            }
             return [action.payload, ...state];
         case REMOVE_FROM_CART:
             // console.log('reducer remove from cart');
