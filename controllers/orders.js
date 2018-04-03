0c3af9e03a078ab6400db0b95ab62183de024d69
Nicolas Riquelme Guzman <nrriquelme@gmail.com>
2018-04-03 16:43:34 -0300

    implementation of the order checkout on backend

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,26 +1,31 @@
 const nodemailer = require('nodemailer');
+const fs = require('fs');
 
 exports.recieveOrder = function(req, res, next){
     //res.json({msg: 'orden recibida!'});
-    let stmpConfig = {
-        service: 'gmail',
-        auth: {
-            user: 'nicolasriquelmeguzman@gmail.com',
-            pass: 'sqrt(-2)notReal!=sqrt(2)==Real'
+    fs.readFile('./config/config.json', function(err, data) {
+        if(err){ res.json({error: 'error al abrir el archivo'}); }
+        data = JSON.parse(data);
+        let stmpConfig = {
+            service: 'gmail',
+            auth: {
+                user: data.user,
+                pass: data.pass
+            }
         }
-    }
 
-    let transporter = nodemailer.createTransport(stmpConfig);
-    let mailOptions = {
-        from: 'Mercadito de Larmahue <mercaditodelarmahue@gmail.com',
-        to: 'olmue2@gmail.com',
-        subject: 'Enviando correos desde Node.js',
-        text: 'Hola mama, este es un correo que se envia desde el mismo servidor del software para probar el envio de correos. Ahora voy a juntarlo con el procesamiento de las ordenes!'
-    };
+        let transporter = nodemailer.createTransport(stmpConfig);
+        let mailOptions = {
+            from: 'Mercadito de Larmahue <mercaditodelarmahue@gmail.com',
+            to: 'nrriquelme@gmail.com',
+            subject: 'Enviando correos desde Node.js',
+            text: 'Correos desde el Backend'
+        };
 
-    transporter.sendMail(mailOptions, function(error, info){
-        if(error){ res.json({error: 'no enviado'});}
-        console.log('Exito en el envio del correo');
-        res.send({msg: 'exito en el envio del correo'});
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) { res.json({ error: 'no enviado' }); }
+            console.log('Exito en el envio del correo');
+            res.send({ msg: 'exito en el envio del correo' });
+        });
     });
 }
