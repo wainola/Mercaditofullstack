0c4feae00250ec8f212385c84e206cfce3a23366
Nicolas Riquelme Guzman <nrriquelme@gmail.com>
2018-04-05 16:53:47 -0300

    success in firts insertion

--- controllers/orders.js
+++ controllers/orders.js
@@ -5,32 +5,50 @@ const db = require('../db');
 exports.recieveOrder = function(req, res, next){
     //res.json({msg: 'orden recibida!'});
     console.log(req.body);
-    // fs.readFile('./config/config.json', function(err, data) {
-    //     if(err){ res.json({error: 'error al abrir el archivo'}); }
-    //     data = JSON.parse(data);
-    //     let stmpConfig = {
-    //         service: 'gmail',
-    //         auth: {
-    //             user: data.user,
-    //             pass: data.pass
-    //         }
-    //     }
+    fs.readFile('./config/config.json', function(err, data) {
+        if(err){ res.json({error: 'error al abrir el archivo'}); }
+        data = JSON.parse(data);
+        let stmpConfig = {
+            service: 'gmail',
+            auth: {
+                user: data.user,
+                pass: data.pass
+            }
+        }
+        let nameFormat = req.body.nombre.split(' ');
+        let nombre = nameFormat[0];
+        let apellidos = `${nameFormat.length > 2 ? `${nameFormat[1]} ${nameFormat[2]}` : nameFormat[1] }`;
+        console.log('name format', nameFormat);
+        const cliente = {
+            nombre: nombre,
+            apellido: apellidos,
+            email: req.body.email,
+        };
+        const orden = {};
 
-    //     let transporter = nodemailer.createTransport(stmpConfig);
-    //     let mailOptions = {
-    //         from: 'Mercadito de Larmahue <mercaditodelarmahue@gmail.com',
-    //         to: 'nrriquelme@gmail.com',
-    //         subject: 'Enviando correos desde Node.js',
-    //         text: 'Correos desde el Backend'
-    //     };
+        db.query('INSERT INTO CLIENTE SET ?', cliente, (err, resultado) => {
+            if(err) { 
+                console.log(err);
+                return;
+            }
+            console.log('datos insertados exitosamente')
+    
+        })
 
-    //     transporter.sendMail(mailOptions, function (error, info) {
-    //         if (error) { res.json({ error: 'no enviado' }); }
-    //         console.log('Exito en el envio del correo');
-    //         res.send({ msg: 'exito en el envio del correo' });
-    //     });
-    // });
-    res.send({ data_recibida: req.body, success: true });
+        // let transporter = nodemailer.createTransport(stmpConfig);
+        // let mailOptions = {
+        //     from: 'Mercadito de Larmahue <mercaditodelarmahue@gmail.com',
+        //     to: 'nrriquelme@gmail.com',
+        //     subject: 'Enviando correos desde Node.js',
+        //     text: 'Correos desde el Backend'
+        // };
+
+        // transporter.sendMail(mailOptions, function (error, info) {
+        //     if (error) { res.json({ error: 'no enviado' }); }
+        //     console.log('Exito en el envio del correo');
+        //     res.send({ data_recibida: req.body, success: true });
+        // });
+    });
 }
 
 exports.dummyOrder = function(req, res, next){
