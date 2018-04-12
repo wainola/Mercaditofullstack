c7c2232bcb32808eb3e18429a3ef53bf0c3c1187
Nicolas Riquelme Guzman <nrriquelme@gmail.com>
2018-04-12 18:21:24 -0300

    remove auth for processing orders

--- router.js
+++ router.js
@@ -23,7 +23,7 @@ module.exports = function (app, CONEXION_MODELOS){
     app.get('/getAllProducts', requireAuth, Productos.getAllProducts);
     app.post('/saveProduct', requireAuth, Productos.saveProduct);
     app.get('/frontAllProducts', Productos.getAllProducts);
-    app.post('/processingOrder', requireAuth, OrdersController.recieveOrder);
+    app.post('/processingOrder', OrdersController.recieveOrder);
     // DUMMY PROCESSING TO TEST SEQUELIZE
     app.post('/dummyCliente', OrdersController.dummyOrder);
 }
